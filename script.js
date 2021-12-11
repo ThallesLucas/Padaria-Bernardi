e1d995f087b475524cc25fc1af733e0073da7716
ThallesLucas <90980021+ThallesLucas@users.noreply.github.com>
2021-12-10 23:16:24 -0300

    Add files via upload

---
+++ script.js
@@ -0,0 +1,25 @@
+
+let time = 8000;
+let currentImage = 0;
+let imagens = document.querySelectorAll("#slider img");
+let max = imagens.length;
+
+function nextImage(){
+    
+    imagens[currentImage].classList.remove("selected")
+
+    currentImage++
+
+    if(currentImage >= max){
+        currentImage = 0
+    }
+
+    imagens[currentImage].classList.add("selected")
+}
+
+
+function start(){
+    setInterval(() => {
+        nextImage()
+    }, time);
+}
